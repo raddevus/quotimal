c2e6443abce58aeb9e0afd48420abed8983dd1f6
raddevus <1radeutsch1@gmail.com>
2024-03-05 17:21:44 -0500

    Added Aniquote.cs (forgot to add before prev commit)

---
+++ api/qapi/Model/Aniquote.cs
@@ -0,0 +1,9 @@
+namespace qapi.Model;
+
+class Aniquote{
+    string Imagelink{get;set;}
+    string InfoLink{get;set;}
+    string Quote{get;set;}
+    string Author{get;set;}
+    string AuthorLink{get;set;}
+}
