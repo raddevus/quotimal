3d257445a263b38b8a183071111aaab90cc61425
raddevus <1radeutsch1@gmail.com>
2024-03-06 18:07:30 -0500

    Added code to load on port 5270 and removed https redirection because that will be handled by nginx.

--- api/qapi/Program.cs
+++ api/qapi/Program.cs
@@ -2,6 +2,11 @@ using qapi.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.ConfigureKestrel((context, serverOptions) =>
+{
+    serverOptions.Listen(System.Net.IPAddress.Loopback, 5270);
+});
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,8 +20,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 var summaries = new[]
 {
